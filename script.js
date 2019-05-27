a3123e3fc0efd76618f4e6dedfcb7e66e9be1d83
“[tine <[tinevancorenland@gmail.com]>
2019-05-27 16:53:28 +0200

    add hover and click functions

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-document.getElementById("doge").innerHTML = "Woof!";
+var button = document.getElementById("doge");
+var image = document.getElementById("bgimage");
+
+button.addEventListener("load", functionLoad);
+button.addEventListener("mouseover", functionMouseover);
+button.addEventListener("mouseout", functionMouseout);
+button.addEventListener("click", functionSwitchImage);
+
+function functionSwitchImage() {
+  image;
+  if (image.style.display === "none") {
+    image.style.display = "block";
+  } else {
+    image.style.display = "none";
+  }
+}
+
+function functionLoad() {
+  button.innerHTML = "Woof";
+}
+
+function functionMouseover() {
+  button.innerHTML = "Woof woof!";
+}
+
+function functionMouseout() {
+  button.innerHTML = "Woof!";
+}
